bdd31578e73ec29fb4e40cef39f42e7e27141f39
aiyuhang <aiyuhang@wityun.com>
2019-07-30 21:11:54 +0800

    初始化gem

---
+++ lib/rails_admin_ui.rb
@@ -0,0 +1,6 @@
+require "rails_admin_ui/version"
+
+module RailsAdminUi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_admin_ui/version.rb
@@ -0,0 +1,3 @@
+module RailsAdminUi
+  VERSION = "0.1.0"
+end
